77350ec6ba8a4b303dba2e0407271c355e9764bb
permoabel15 <105014433+permoabel15@users.noreply.github.com>
2023-02-01 00:40:38 -0600

    first commit

---
+++ script1.py
@@ -0,0 +1,16 @@
+import os
+import re
+import subprocess
+
+ips = []
+output = os.popen("nmap -sP 192.168.0.0/24").read()
+
+# Lista de direcciones IP
+ips = re.findall(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})", output)
+
+# Nombre del script a ejecutar
+script_name = "run_script.sh"
+
+# Ejecuta el script en cada dirección IP
+for ip in ips:
+    subprocess.call(["ssh", "user@" + ip, "sh", script_name])
